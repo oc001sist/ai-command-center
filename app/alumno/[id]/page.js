8e686a676e64e2bf03434ddd0d9f85652266ccaa
mrjuarez1contactos <mrjuarez1contactos@gmail.com>
2026-06-02 18:48:46 -0700

    botón ABRIR TERMINAL en panel alumno

--- app/alumno/[id]/page.js
+++ app/alumno/[id]/page.js
@@ -356,6 +356,7 @@ export default function AlumnoPage({ params }) {
                 >
                   ⛔ APAGAR VM
                 </button>
+                <a href={id === 'oc002' ? 'https://cloud.digitalocean.com/droplets/573384694/terminal' : 'https://cloud.digitalocean.com/droplets/573643029/terminal'} target="_blank" rel="noreferrer" className="btn btn-blue" style={{ width: '100%', padding: '18px', fontSize: '17px', textAlign: 'center', marginTop: '10px', display: 'block' }}>🖥️ ABRIR TERMINAL</a>
               )}
             </div>
           </div>
